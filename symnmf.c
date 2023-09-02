b4e5104e5bdebddb0e304b9950adbe10f95c911d
dory1110 <dory.grossman@digitalturbine.com>
2023-09-02 23:51:39 +0300

    fixes of makefile

--- symnmf.c
+++ symnmf.c
@@ -1,12 +1,14 @@
 #include "symnmf.h"
 
 int main(int argc, char** argv) {
-    int rows, cols;
     MAT* matrix = readCSVtoMatrix(argv[2]);
+    if (argc > 2){
+        exit(1);
+    }
 
-    if (argv[1] == "sym"){printMat(createSymMat(matrix));}
-    else if (argv[1] == "ddg"){printMat(createDdgMat(matrix));}
-    else if (argv[1] == "norm"){printMat(createNsmMat(matrix));}
+    if (strcmp(argv[1], "sym")){printMat(createSymMat(matrix));}
+    else if (strcmp(argv[1], "ddg")){printMat(createDdgMat(matrix));}
+    else if (strcmp(argv[1], "norm")){printMat(createNsmMat(matrix));}
     else{
         printf("An Error has Occured");
         exit(1);
