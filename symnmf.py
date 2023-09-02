4a6a366db1d711e51d0d09c88bb26b3df238b64c
Avishay Spitzer <a_spitzer@apple.com>
2023-09-02 22:42:54 +0300

    initial structure

--- symnmf.py
+++ symnmf.py
@@ -3,6 +3,9 @@ import numpy as np
 import symnmfssp
 np.random.seed(0)
 
+MAX_ITER = 300
+EPS = 10 ** (-4)
+
 "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ INPUT HANDLING ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 
@@ -46,7 +49,7 @@ def file_to_vectors(file_path):
     return vectors
 
 
-def init_H(n, k, m):
+def init_h(n, k, m):
     """
     :param n:   num of vecs
     :param k:   num of clusters
@@ -54,21 +57,42 @@ def init_H(n, k, m):
     :return:    H matrix
     """
     high = 2 * (m / k) ** 2
-    print(high)
     return np.random.uniform(high=high, size=(n, k))
+
+
+def _calc_m(w):
+    total_sum = 0
+    count = 0
+    for vector in w:
+        for value in vector:
+            total_sum += value
+            count += 1
+    return total_sum / count if count != 0 else 0
+
+
+def print_vectors(centroids_list):
+    for centroid in centroids_list:
+        print(",".join([str(x) for x in centroid]))
+    print()
 
 
 def symnmf_main(analysis=False, k=3, goal=GoalEnum.symnmf, vectors_list=None):
     if not analysis:
         k, goal, vectors_list = get_input()
     if goal == GoalEnum.symnmf:
-        pass
+        w = symnmfssp.norm(vectors_list, len(vectors_list), len(vectors_list[0]))
+        h = init_h(len(vectors_list), k, _calc_m(w))
+        result = symnmfssp.symnmf(h, len(h), len(h[0]), w, len(w), len(w[0]), MAX_ITER, EPS)
     elif goal == GoalEnum.sym:
-        pass
+        result = symnmfssp.sym(vectors_list, len(vectors_list), len(vectors_list[0]))
     elif goal == GoalEnum.ddg:
-        pass
+        result = symnmfssp.ddg(vectors_list, len(vectors_list), len(vectors_list[0]))
     elif goal == GoalEnum.norm:
-        pass
+        result = symnmfssp.norm(vectors_list, len(vectors_list), len(vectors_list[0]))
+    else:
+        print("an error has occurred")
+        raise Exception
+    print_vectors(result)
 
 
 if __name__ == '__main__':
