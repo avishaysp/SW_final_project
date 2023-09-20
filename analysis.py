967761579a71acc473cee3a01272f3c3488501df
Avishay Spitzer <a_spitzer@apple.com>
2023-09-20 22:54:39 +0300

    cleaned python files

--- analysis.py
+++ analysis.py
@@ -1,6 +1,4 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.metrics import silhouette_score
 import sys
 from hw1 import hw1_main
@@ -8,7 +6,7 @@ from symnmf import symnmf_main, GoalEnum
 
 MAX_ITER = 300
 EPS = 10 ** (-4)
-K = 5
+K = 3
 
 
 def get_input():
@@ -45,15 +43,12 @@ def main():
 def print_silhouette_nmf(nmf, vectors_list):
     labels = get_highest_value_indices(nmf)
     this_ss = silhouette_score(vectors_list, labels)
-    plot_clusters(labels, vectors_list, "nmf")
-    print(f'nmf: {round(this_ss, 16)}')
-
+    print(f'nmf: {this_ss:.4f}')
 
 def print_silhouette_kmeans(centroids, vectors_list):
     labels = np.array([assign_to_nearest_centroid(v, centroids) for v in vectors_list])
     this_ss = silhouette_score(vectors_list, labels)
-    plot_clusters(labels, vectors_list, "kmeans")
-    print(f'kmeans: {round(this_ss, 16)}')
+    print(f'kmeans: {this_ss:.4f}')
 
 
 def assign_to_nearest_centroid(sample, centroids):
@@ -63,25 +58,6 @@ def assign_to_nearest_centroid(sample, centroids):
 
 def get_highest_value_indices(vectors):
     return np.argmax(vectors, axis=1)
-
-
-def plot_clusters(labels, vectors_list, title):
-    tab1 = pd.DataFrame(vectors_list)
-    tab1['columns'] = labels
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    x = np.array(tab1[0])
-    y = np.array(tab1[1])
-    z = np.array(tab1[2])
-
-    ax.scatter(x, y, z, marker="o", c=tab1["columns"], s=1, cmap="viridis")
-
-    ax.set_xlim(tab1[0].min() - 1, tab1[0].max() + 1)  # Replace x_min and x_max with your desired limits
-    ax.set_ylim(tab1[1].min() - 1, tab1[1].max() + 1)  # Replace y_min and y_max with your desired limits
-    ax.set_zlim(tab1[2].min() - 1, tab1[2].max() + 1)
-    plt.title(title)
-    plt.show()
 
 
 if __name__ == '__main__':
