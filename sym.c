3ea52df34e1cc6b80c2c7e4f547cb8ec1080b198
dory1110 <dory.grossman@digitalturbine.com>
2023-09-21 00:51:46 +0300

    remove prints

--- sym.c
+++ sym.c
@@ -5,8 +5,6 @@ static double euclidianDistance(double* vector1, double* vector2, int length) {
     int i;
     for (i = 0; i < length; i++) {
         sum += pow(vector1[i] - vector2[i], 2);
-
-        printf("%f %f\n", vector1[i], vector2[i]);
     }
     return sum;
 }
@@ -16,17 +14,12 @@ MAT* createSymMat(MAT* points){
     MAT* symMat = initMat(numOfVectors, numOfVectors);
     int i,j;
     double val;
-    printMat(points);
-    printf("#############\n");
-
     for (i = 0; i < numOfVectors; i++){
         for (j = 0; j < numOfVectors; j++){
             if(i == j){
                 symMat->vals[i][j] = 0;
             } else{
                 val = -0.5 * euclidianDistance(points->vals[i], points->vals[j], points->VECTORS_LENGTH);
-                printf("%d %d %f\n", i, j, val);
-                printf("#############\n");
                 symMat->vals[i][j] = exp(val);
             }
         }
